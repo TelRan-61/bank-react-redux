0d65c0c6f66417764bc8f5a19ec9fe7a4c9e2386
edward <edwardlfish@gmail.com>
2025-08-10 19:33:33 +0300

    Quote View

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -6,6 +6,7 @@ const Balance = () => {
     return (
         <div className={'text-center text-uppercase'}>
             <h1 className={'text-[gold] font-bold text-8xl'}>Iron Bank of Braavos</h1>
+            <h3 className={'text-[whitesmoke] text-5xl'}>Winter is coming...</h3>
             <h2 className={'text-[firebrick] text-7xl'}>Balance = {balance}</h2>
         </div>
     );
--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -7,20 +7,27 @@ const Operation = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className={'flex justify-center'}>
-            <button
-                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg rounded-lg py-2 px-4'}
-                onClick={() => dispatch(withdraw(sum))}>Withdraw</button>
-            <input
-                className={'text-center border rounded-lg text-white text-lg font-bold'}
-                type="number"
-                onChange={e => setSum(+e.target.value)}
-                value={sum}
-            />
-            <button
-                className={'bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg rounded-lg py-2 px-4'}
-                onClick={() => dispatch(deposit(sum))}>Deposit</button>
-        </div>
+        <>
+            <div className={'flex justify-center'}>
+                <button
+                    className={'bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg rounded-lg py-2 px-4'}
+                    onClick={() => dispatch(withdraw(sum))}>Withdraw
+                </button>
+                <input
+                    className={'text-center border rounded-lg text-white text-lg font-bold'}
+                    type="number"
+                    onChange={e => setSum(+e.target.value)}
+                    value={sum}
+                />
+                <button
+                    className={'bg-blue-500 hover:bg-blue-600 text-white font-bold text-lg rounded-lg py-2 px-4'}
+                    onClick={() => dispatch(deposit(sum))}>Deposit
+                </button>
+            </div>
+            <div className={'flex justify-center'}>
+                <button className={'bg-blue-300 hover:bg-blue-400 text-white font-bold text-lg rounded-lg px-9 py-2 mt-1'}>Get Quote</button>
+            </div>
+        </>
     );
 };
 
